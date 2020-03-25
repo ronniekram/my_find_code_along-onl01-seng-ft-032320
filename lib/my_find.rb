8cf8e10e390accf5b86d2747fa60e088cfdbf00e
Arynn Boniface <arynn.boniface@gmail.com>
2020-03-25 19:09:24 +0000

    Done.

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,11 @@
 require 'pry'
-
+#method
 def my_find(collection)
-
+  #counter
+  i = 0
+  #while loops through array
+  while i < collection.length
+    return collection[i] if yield(collection[i])
+    i += 1
+  end
 end
