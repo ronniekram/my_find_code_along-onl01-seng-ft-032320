7db076d06b41b972cf1d4d9575b3925719b2c2a2
Arynn Boniface <arynn.boniface@gmail.com>
2020-03-25 19:06:22 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,9 +1,13 @@
 require 'pry'
-
+#method
 def my_find(collection)
+  #counter
   i = 0
+  #while loops through array
   while i < collection.length
+  #stops at each element 
     yield(collection[i])
+    #counts through array indexes
     i += 1
   end
 end
